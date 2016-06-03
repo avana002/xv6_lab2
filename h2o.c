8b4c196e8c9c0a96d7a12779a5ce679e6547443f
Atticus Vanarsdale <avana002@sledge.cs.ucr.edu>
2016-06-03 15:59:59 -0700

    got h2o working

--- h2o.c
+++ h2o.c
@@ -1,48 +1,50 @@
 #include "semaphore.c"
 #include "thread.h"
-#include "user.h"
-//#include "queue.c"
-//#include <stdio.h>
 
 void hReady(void*);
 void oReady(void*);
 
 int water = 0;
 
-//struct Semaphore h,o,l;
+Semaphore h;
+Semaphore o;
+Semaphore l;
 
 int main()
 {
-/*
-   //Semaphore h,o,l;
+
    Sem_init(&h, 0);
    Sem_init(&o, 0);
    Sem_init(&l, 1);
 
+
    thread_create(hReady,(void*)&water);
    thread_create(hReady,(void*)&water);  
    thread_create(oReady,(void*)&water);
-*/
+   
+   //main waits for threads to exit   
+   while(wait() >= 0);
+
    exit();
 }
 
 void hReady(void* v)
 {
- //  sem_signal(&h);
- //  sem_acquire(&o);
- texit();
+   sem_signal(&h);
+   sem_acquire(&o);
+   texit();
 }
 
 void oReady(void* v)
-{/*
+{
    sem_acquire(&h);
    sem_acquire(&h);
    sem_signal(&o);
    sem_signal(&o);
    sem_acquire(&l);
    water++;
-   //printf(0,"water created\n");
+   printf(1,"water created\n");
    sem_signal(&l);
-*/
-  texit();
+
+   texit();
 }
--- semaphore.c
+++ semaphore.c
@@ -1,4 +1,6 @@
 #include "queue.h"
+#include "types.h"
+#include "user.h"
 
 #ifndef _SEMAPHORE_
 #define _SEMAPHORE_
@@ -14,6 +16,11 @@ struct Semaphore
 
 typedef struct Semaphore Semaphore;
 
+int sem_count(Semaphore* s)
+{
+   return s->count;
+}
+
 void Sem_init(Semaphore *s, int v)
 {
    s->count = v;
@@ -25,13 +32,16 @@ void sem_acquire(Semaphore *s)
    //if count is positive, decrement count
    if(s->count > 0)
    {
+     // printf(1,"count is positive\n");
       (s->count)--;
    }
    //if count is zero, thread added to queue and waits
    else
    {
-      add_q(&(s->q),getpid());
-      while(s->count == 0 || front(&(s->q)) != getpid()) wait();
+    // printf(1,"count is zero\n"); 
+     add_q(&(s->q),getpid());
+      while(s->count == 0 || (front(&(s->q)) != getpid())) wait();
+      pop_q(&(s->q));
    }
 }
 
